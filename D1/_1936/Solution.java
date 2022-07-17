282ec76d514f5f192ae56b5b1148bc9980bd97d5
johan kong <johankkong@gmail.com>
2022-07-17 11:51:36 +0900

    1936 add

---
+++ D1/_1936/Solution.java
@@ -0,0 +1,32 @@
+package D1._1936;
+
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		if (A == 1) {
+			if (B == 2) {
+				System.out.println("B");
+			} else { 
+				System.out.println("A"); 
+			}
+		} else if (A == 2) {
+			if (B == 3) {
+				System.out.println("B");
+			} else { 
+				System.out.println("A"); 
+			}
+		} else {
+			if (B == 1) {
+				System.out.println("B");
+			} else { 
+				System.out.println("A"); 
+			
+			}
+		}
+		sc.close();
+	}
+}
