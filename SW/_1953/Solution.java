00bd9f0262b62ec53f9df64eb5b18269cb6a9e47
johankong <johankkong@gmail.com>
2022-10-13 17:59:10 +0900

    1013 solving

---
+++ SW/_1953/Solution.java
@@ -0,0 +1,34 @@
+package SW._1953;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(in.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(in.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			int R = Integer.parseInt(st.nextToken());
+			int C = Integer.parseInt(st.nextToken());
+			int L = Integer.parseInt(st.nextToken());
+			int[][] arr = new int[N][N];
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(in.readLine());
+				for (int j = 0; j < N; j++) {
+					arr[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			
+		}
+	}
+	
+	static int[] dr = {-1, 1, 0, 0};
+	static int[] dc = {0, 0, -1, 1};
+	
+}
